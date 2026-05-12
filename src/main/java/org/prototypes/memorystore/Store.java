c0dec8b0083b33e913b9375325bd9626436272c9
Rahul Das <rahuldas.rr81@gmail.com>
2026-05-12 17:04:01 +0530

    Added a basic memory store (#1)
    * Added a basic memory store
    
    * Used native java Object to store values in memstore

---
+++ src/main/java/org/prototypes/memorystore/Store.java
@@ -0,0 +1,49 @@
+package org.prototypes.memorystore;
+
+import java.util.HashMap;
+
+public class Store {
+
+    private final HashMap<String, Object> store;
+
+    public Store() {
+//        Demonstration for using complex object class as value in the hashmap, Object class be directly used as value type in HashMap in Java as present in the active code.
+//        store = new HashMap<String, ValueType>();
+        store = new HashMap<String, Object>();
+    }
+
+    public void put(String key, Object value){
+//        store.put(key, new ValueType(value.getClass(), value));
+        store.put(key, value);
+    }
+
+    public Class<?> getType(String key){
+        return store.get(key).getClass();
+    }
+
+    public Object get(String key){
+        //        ValueType data = store.get(key);
+        //        if (data == null) {return null;}
+        //        return  data.getData();
+        return store.get(key);
+    }
+
+    public void remove(String key) {
+        store.remove(key);
+        System.out.println(key + " Key Removed !");
+    }
+    public static void main (String[] args) {
+
+        Store memstore = new Store();
+        memstore.put("Test", "Success!");
+        memstore.put("TestInt", 111);
+
+        System.out.println(memstore.get("Test"));
+        System.out.println(memstore.get("TestInt"));
+
+        memstore.remove("Test");
+        memstore.remove("TestInt");
+        System.out.println(memstore.get("Test"));
+
+    }
+}
